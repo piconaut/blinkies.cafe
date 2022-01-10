cc97265855b74c8dab97a362a33aabac2c09c743
Amy Fare <amycfare@gmail.com>
2022-01-10 13:07:38 -0500

    client blinkiegen tweak

--- src/client/blinkiegen.js
+++ src/client/blinkiegen.js
@@ -1,9 +1,19 @@
 /*jslint browser */
 
-async function getBlinkieList() {
+function getBlinkieList() {
     return fetch('/blinkieList.json')
         .then((response)=>response.json())
         .then((responseJson)=>{return responseJson});
+}
+
+function postBlinkie(blinkieText, blinkieStyle) {
+    return fetch("/api/blinkiegen", {
+        body: JSON.stringify({blinkieText: blinkieText, blinkieStyle: blinkieStyle}),
+        headers: {"Content-Type": "application/json"},
+        method: "POST"
+    })
+        .then((res) => res.text())
+        .then(blinkieURL => { return blinkieURL; })
 }
 
 function enterSubmit(event){
@@ -13,38 +23,33 @@ function enterSubmit(event){
     }
 }
 
-let submit = async function (event) {
+let submit = function (event) {
     event.preventDefault();
     let blinkieGIF = document.getElementById("blinkieGIF");
-    let blinkieTextVal = document.getElementById("blinkieText").value;
-    let blinkieStyleVal = document.getElementById("blinkieStyle").value;
+    let blinkieText = document.getElementById("blinkieText").value;
+    let blinkieStyle = document.getElementById("blinkieStyle").value;
     let blinkieLinkHolder = document.getElementById('blinkieLinkHolder');
 
-    let res = await fetch("/api/blinkiegen", {
-        body: JSON.stringify({blinkieText: blinkieTextVal, blinkieStyle: blinkieStyleVal}),
-        headers: {"Content-Type": "application/json"},
-        method: "POST"
+    postBlinkie(blinkieText, blinkieStyle).then( function(blinkieURL) {
+        blinkieGIF.src = blinkieURL;
+        blinkieLinkHolder.innerHTML = 'blinkie link: <br>';
+        let blinkieLink = document.createElement('a');
+        blinkieLink.innerHTML = blinkieURL.split('//')[1];
+        blinkieLink.href = blinkieURL;
+        blinkieLink.target = "_blank";
+        blinkieLinkHolder.appendChild(blinkieLink);
     });
-    let blinkieURL = await res.text()
-
-    blinkieGIF.src = blinkieURL;
-    blinkieLinkHolder.innerHTML = 'blinkie link: <br>';
-    let blinkieLink = document.createElement('a');
-    blinkieLink.innerHTML = blinkieURL.split('//')[1];
-    blinkieLink.href = blinkieURL;
-    blinkieLink.target = "_blank";
-    blinkieLinkHolder.appendChild(blinkieLink);
 };
 
 let styleSelect = document.getElementById('blinkieStyle');
-const blinkieList = await getBlinkieList();
-
-for (const [key, value] of Object.entries(blinkieList)) {
-    let opt = document.createElement('option');
-    opt.value = key;
-    opt.innerHTML = value.name;
-    styleSelect.appendChild(opt);
-}
+const blinkieList = getBlinkieList().then(function(blinkieList){
+    for (const [key, value] of Object.entries(blinkieList)) {
+        let opt = document.createElement('option');
+        opt.value = key;
+        opt.innerHTML = value.name;
+        styleSelect.appendChild(opt);
+    }
+});
 
 document.getElementById("blinkieForm").addEventListener("submit", submit);
 document.getElementById("blinkieText").addEventListener("keypress", enterSubmit);

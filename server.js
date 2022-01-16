52d358b6d142061e2c3ee1b355fe507ff38a53cc
Amy Fare <amycfare@gmail.com>
2022-01-16 12:16:01 -0500

    trim api pour options

--- server.js
+++ server.js
@@ -262,7 +262,7 @@ app.options("/styleList.json", function(req, res, next){
 
 app.options("/api/pour", function(req, res, next){
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'POST,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
   res.sendStatus(200);
 });

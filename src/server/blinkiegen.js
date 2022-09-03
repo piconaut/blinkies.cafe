e2b5c6eb376465a178b6522bf28df4050bb91257
Amy Fare <amycfare@gmail.com>
2022-09-03 17:22:45 -0400

    add option for split default text

--- src/server/blinkiegen.js
+++ src/server/blinkiegen.js
@@ -267,7 +267,9 @@ async function pour(blinkieID, instyle, intext, inscale, split) {
             'x2':         blinkieData.styleProps[styleID].x,
             'y2':         0,
             'unicodeCharCodes': '',
-            'split':      split
+            'split':      split,
+            'splitDefault': blinkieData.styleProps[styleID].splitDefault
+                ? blinkieData.styleProps[styleID].splitDefault : false
         };
 
         blinkieLink = siteURL + '/b/blinkiesCafe-' + blinkieID + '.gif';
@@ -276,6 +278,7 @@ async function pour(blinkieID, instyle, intext, inscale, split) {
         bParms.cleantext = replaceChars(bParms.intext);
         if (bParms.cleantext.replace(/\s/g, '').length == 0) {
             bParms.cleantext = replaceChars(blinkieData.styleProps[bParms.styleID].name);
+            bParms.split = bParms.splitDefault;
         }
 
         // prepare text for rendering, then render frames and generate gif.

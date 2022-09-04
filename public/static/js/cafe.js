072eeee658d040adf3c93b37485a3932c32003d3
Amy Fare <amycfare@gmail.com>
2022-09-04 00:29:43 -0400

    input symbols directly into input text. slash codes deprecated

--- public/static/js/cafe.js
+++ public/static/js/cafe.js
@@ -85,8 +85,6 @@ function selectStyle(styleList, targetKey) {
 
     let blinkieText = document.getElementById('blinkieText');
     const tips = ['your text here!',
-                  'type /heart \u2665',
-                  'type /eheart \u2661',
                   'make smth cool :D',
                   'trans rights!!'];
     blinkieText.placeholder = tips[Math.floor(Math.random()*tips.length)];
@@ -305,6 +303,49 @@ symbolToggle.onclick = function() {
         symbolTable.style.visibility = 'hidden';
         symbolToggle.innerText = 'show symbols ▾';
     }
+}
+
+let symbolButtons = document.querySelectorAll(".symbolButton");
+symbolButtons.forEach(function(button){
+    button.onclick = function() {
+        insertAtCaret('blinkieText',button.innerText);
+    }
+});
+
+// https://web.archive.org/web/20110102112946/http://http://www.scottklarr.com/topic/425/how-to-insert-text-into-a-textarea-where-the-cursor-is/
+function insertAtCaret(areaId,text) {
+    var txtarea = document.getElementById(areaId);
+    var scrollPos = txtarea.scrollTop;
+    var strPos = 0;
+    var br = ((txtarea.selectionStart || txtarea.selectionStart == '0') ?
+        "ff" : (document.selection ? "ie" : false ) );
+    var range;
+    if (br == "ie") {
+        txtarea.focus();
+        range = document.selection.createRange();
+        range.moveStart ('character', -txtarea.value.length);
+        strPos = range.text.length;
+    }
+    else if (br == "ff") strPos = txtarea.selectionStart;
+
+    var front = (txtarea.value).substring(0,strPos);
+    var back = (txtarea.value).substring(strPos,txtarea.value.length);
+    txtarea.value=front+text+back;
+    strPos = strPos + text.length;
+    if (br == "ie") {
+        txtarea.focus();
+        range = document.selection.createRange();
+        range.moveStart ('character', -txtarea.value.length);
+        range.moveStart ('character', strPos);
+        range.moveEnd ('character', 0);
+        range.select();
+    }
+    else if (br == "ff") {
+        txtarea.selectionStart = strPos;
+        txtarea.selectionEnd = strPos;
+        txtarea.focus();
+    }
+    txtarea.scrollTop = scrollPos;
 }
 
 document.getElementById("badgetxt").onclick = function() {

26b6f304e503220d07c6b6fd0927fba4650194e9
Amy Fare <amycfare@gmail.com>
2022-11-20 13:49:00 -0500

    also search default/alt text. hide non-matching blinkies.

--- public/static/js/wall.js
+++ public/static/js/wall.js
@@ -1,27 +1,18 @@
-let blinkies = document.querySelectorAll("#tiles a");
+const blinkies = document.querySelectorAll("#tiles a");
+const search = document.getElementById("search");
 
 function unspecify(string) {
     return string.toLowerCase().replaceAll(" ", "");
 }
 
 function searchBlinkies() {
-    let prompt = unspecify(search.value);
-    console.log(prompt)
+    const prompt = unspecify(search.value);
     for (let i = 0; i < blinkies.length; i++) {
-
-        let blinkie = blinkies[i];
-        if (unspecify(blinkie.id).indexOf(prompt) > -1) {
-            blinkie.style.opacity = "1";
-            // Depending on what you prefer, you can also use .display to fully hide the other ones!
-            //blinkie.style.display = "initial";
-        }
-        else {
-            blinkie.style.opacity = "0.2";
-            //blinkie.style.display = "none";
-        }
+        const blinkie = blinkies[i];
+        if (unspecify(blinkie.id).indexOf(prompt) > -1) blinkie.style.display = "initial";
+        else blinkie.style.display = "none";
     }
 }
-
 
 search.addEventListener("input", searchBlinkies);
 search.addEventListener("keyup", searchBlinkies);
